5f826db6a5884057c3f6a98085143d70c6ec43f9
«Nikolay <>
2022-06-29 21:09:26 +0600

    zadacha3

---
+++ Задача 3/Program.cs
@@ -0,0 +1,8 @@
+﻿// 3. Напишите программу вычисления функции:
+ //x = f(a)
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int resF = number*number*number;
+
+Console.WriteLine($"Куб числа {number} равен: {resF}");
