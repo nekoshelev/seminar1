a4655f187d638f88d7517542e88b6c159a2139d3
«Nikolay <>
2022-06-29 21:43:22 +0600

    zadacha5

---
+++ task5/Program.cs
@@ -0,0 +1,16 @@
+﻿// *. Напишите программу вычисления модуля числа.
+//	2 -> 2
+//	-3 -> 3
+//	-7 -> 7
+
+Console.WriteLine("Введите число: ");
+int numN = int.Parse(Console.ReadLine());
+
+if (numN > 0)
+{
+    Console.WriteLine($"Модуль числа {numN} равен: {numN}");
+}
+else
+{
+    Console.WriteLine($"Модуль числа {numN} равен: {-numN}");
+}
